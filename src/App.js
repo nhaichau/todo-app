62602cd502e9b77c5879af23a48334d41d0fe5da
Chau H. Nguyen <nhaichau@gmail.com>
2019-12-05 15:01:05 +0700

    Fisrt commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import TodoItem from "./TodoItem";
+import todoData from "./todoData";
+
+function App() {
+    const todoItems = todoData.map(item => (
+        <TodoItem
+            key={item.id}
+            item={item}
+        />
+    ));
+    return (
+        <div className="todo-list">
+            {todoItems}
+        </div>
+    )
+}
+
+export default App
---
+++ src/todoData.js
@@ -0,0 +1,30 @@
+const todoData = [
+    {
+        id: 1,
+        text: "task1",
+        completed: true
+    },
+    {
+        id: 2,
+        text: "task2",
+        completed: false
+    },
+    {
+        id: 3,
+        text: "task3",
+        completed: true
+    },
+    {
+        id: 4,
+        text: "task4",
+        completed: false
+    },
+    {
+        id: 5,
+        text: "task5",
+        completed: false
+    },
+
+];
+
+export default todoData;
